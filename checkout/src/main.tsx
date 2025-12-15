b5b854cc3065e778c8cfe344d9d1768b9fcb2e2f
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2025-12-14 23:46:05 -0300

    fix: remove toaster component

--- checkout/src/main.tsx
+++ checkout/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode, lazy, Suspense } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -6,16 +6,16 @@ import { BrowserRouter } from "react-router-dom";
 import { ErrorBoundary } from "./components/ui/ErrorBoundary";
 
 // Lazy load do Toaster para evitar problemas de inicialização circular
-const Toaster = lazy(() => import("./components/ui/Toaster").then(mod => ({ default: mod.Toaster })));
+// const Toaster = lazy(() => import("./components/ui/Toaster").then(mod => ({ default: mod.Toaster })));
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ErrorBoundary>
       <BrowserRouter>
         <App />
-        <Suspense fallback={null}>
+        {/* <Suspense fallback={null}>
           <Toaster />
-        </Suspense>
+        </Suspense> */}
       </BrowserRouter>
     </ErrorBoundary>
   </StrictMode>
